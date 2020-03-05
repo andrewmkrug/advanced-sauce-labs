deb96561e471b1e4e842c25d9ee5e73ef9886c80
Andrew Krug <andrew@lazycoder.io>
2020-03-05 13:46:33 -0500

    updating checkout feature to include old version of checkout test

--- workshop/Exercises/CheckoutFeatureTest.cs
+++ workshop/Exercises/CheckoutFeatureTest.cs
@@ -29,5 +29,28 @@ namespace workshop.Exercises
             // assert that the test is finished by checking the last page's URL
             Assert.True(completePage.IsLoaded());
         }
+        
+        [Test]
+        public void ShouldBeAbleToCheckoutWithItems_Before() {
+             
+            //navigate to the url of the Sauce Labs Sample app
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.visit();
+
+            // Ignore the following selectors
+            String username = "standard_user";
+            String password = "secret_sauce";
+            InventoryPage inventoryPage = loginPage.login(username, password);
+
+            // Assert that the url is on the inventory page
+            //TODO fix this assertion later
+            Assert.AreEqual("https://www.saucedemo.com/inventory.html", driver.Url);
+            inventoryPage.addBackpackToCart();
+            ShoppingCartPage cart = inventoryPage.goToShoppingCart();
+            CheckoutStepTwoPage stepTwoPage = cart.checkout();
+            ConfirmationPage confirmationPage = stepTwoPage.fillOutInformation("first", "last", "zip");
+            CheckoutCompletePage finalConfirmationPage = confirmationPage.finish();
+            Assert.True(finalConfirmationPage.IsLoaded());
+        }
     }
 }
